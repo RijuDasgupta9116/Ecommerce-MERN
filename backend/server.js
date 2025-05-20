d56e7430071a1f1470a8c4a92828e8d95e9594f5
RijuDasgupta9116 <riju.dasgupta@gcettb.ac.in>
2025-05-20 22:53:18 +0530

    backend setup and primary module installing is done

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+require('dotenv').config();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
